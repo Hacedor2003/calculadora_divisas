038c114da400076945f997cae141a268fffbe8e5
Hacedor2003 <bryanlenier@gmail.com>
2024-01-30 21:28:11 -0500

    Optimizacion de estilos

--- app/calculadora/page.tsx
+++ app/calculadora/page.tsx
@@ -1,12 +1,19 @@
 'use client';
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import { Card } from '../UI/Card';
 import estilos from '../estilos.module.css';
 import { ContextData } from '../Components/ContextProvider';
 
 export default function Page() {
 	const moneda = useContext(ContextData);
-	const [valor, setValor] = useState(1);
+	const [valor, setValor] = useState(null);
+
+	const handleInputChange = (e: any) => {
+		const inputValue = e.target.valueAsNumber;
+		if (inputValue >= 0 || Number.isNaN(inputValue)) {
+			setValor(inputValue);
+		}
+	};
 
 	return (
 		<>
@@ -16,29 +23,29 @@ export default function Page() {
 				<input
 					type='number'
 					id='calculadora'
-					value={valor}
-					onChange={(e) => setValor(e.target.valueAsNumber || 1)}
+					value={valor ? valor : ''}
+					onChange={handleInputChange}
 				/>
 			</article>
 			<section>
 				<Card
-					dinero={`${moneda ? (valor * moneda.usd).toFixed(2) + ' CUP' : 'Cargando'}`}
-					dinero2={`${moneda ? ((valor * moneda.usd) / moneda.euro).toFixed(2) + ' EUROS' : 'Cargando'}`}
-					dinero3={`${moneda ? ((valor * moneda.usd) / moneda.mlc).toFixed(2) + ' MLC' : 'Cargando'}`}
+					dinero={`${moneda ? (valor ? valor * moneda.usd : 0).toFixed(2) + ' CUP' : 'Cargando'}`}
+					dinero2={`${moneda ? ((valor ? valor * moneda.usd : 0) / moneda.euro).toFixed(2) + ' EUROS' : 'Cargando'}`}
+					dinero3={`${moneda ? ((valor ? valor * moneda.usd : 0) / moneda.mlc).toFixed(2) + ' MLC' : 'Cargando'}`}
 					image='/usa.webp'
 					moneda='USD'
 				/>
 				<Card
-					dinero={`${moneda ? (valor * moneda.euro).toFixed(2) + ' CUP' : 'Cargando'}`}
-					dinero2={`${moneda ? ((valor * moneda.euro) / moneda.usd).toFixed(2) + ' USD' : 'Cargando'}`}
-					dinero3={`${moneda ? ((valor * moneda.euro) / moneda.mlc).toFixed(2) + ' MLC' : 'Cargando'}`}
+					dinero={`${moneda ? (valor ? valor * moneda.euro : 0).toFixed(2) + ' CUP' : 'Cargando'}`}
+					dinero2={`${moneda ? ((valor ? valor * moneda.euro : 0) / moneda.usd).toFixed(2) + ' USD' : 'Cargando'}`}
+					dinero3={`${moneda ? ((valor ? valor * moneda.euro : 0) / moneda.mlc).toFixed(2) + ' MLC' : 'Cargando'}`}
 					image='/euro.webp'
 					moneda='EURO'
 				/>
 				<Card
-					dinero={`${moneda ? (valor * moneda.mlc).toFixed(2) + ' CUP' : 'Cargando'}`}
-					dinero2={`${moneda ? ((valor * moneda.mlc) / moneda.euro).toFixed(2) + ' EUROS' : 'Cargando'}`}
-					dinero3={`${moneda ? ((valor * moneda.mlc) / moneda.usd).toFixed(2) + ' USD' : 'Cargando'}`}
+					dinero={`${moneda ? (valor ? valor * moneda.mlc : 0).toFixed(2) + ' CUP' : 'Cargando'}`}
+					dinero2={`${moneda ? ((valor ? valor * moneda.mlc : 0) / moneda.euro).toFixed(2) + ' EUROS' : 'Cargando'}`}
+					dinero3={`${moneda ? ((valor ? valor * moneda.mlc : 0) / moneda.usd).toFixed(2) + ' USD' : 'Cargando'}`}
 					image='/mlc.jpeg'
 					moneda='MLC'
 				/>
