c351ab1d2e5093208f4093a647656fdb1ce2d259
Hacedor2003 <bryanlenier@gmail.com>
2024-02-04 16:43:36 -0500

    Probando service Workers

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import { BarraNavegacion } from './UI/BarraNavegacion';
-import estilos from './estilos.module.css';
+'use client'
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 export default function Home() {
+	useEffect(() => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/service-worker.js').then((registration) => console.log('scope is: ', registration.scope));
+		}
+	}, []);
 	return (
 		<section>
 			<Image
